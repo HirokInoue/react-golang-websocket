279138293cfea048716215b021ba27db1676e906
HirokInoue <70567194+HirokInoue@users.noreply.github.com>
2021-10-10 23:25:58 +0900

    RethinkDBの接続処理を作成

---
+++ api/infra/rethinkdb.go
@@ -0,0 +1,16 @@
+package infra
+
+import (
+	r "github.com/dancannon/gorethink"
+)
+
+func NewSession(dbName string) (*r.Session, error) {
+	session, err := r.Connect(r.ConnectOpts{
+		Address:  "localhost:28015",
+		Database: dbName,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return session, nil
+}
